6135af2bdf75fd41037eb0dbebac1e65b67bff6e
Ilia Volkov <ilya.j0b@yandex.ru>
2019-04-26 21:24:19 +0300

    create layout of horizontal videocard

---
+++ src/components/VideoCardHor/VideoCardHor.js
@@ -0,0 +1,32 @@
+import React from "react";
+import card from "./VideoCardHor.scss";
+
+const VideoCardHor = () => {
+  return (
+    <div className={card.container}>
+      <div className={card.card}>
+        <div className={card.img}>
+          <img src="https://fakeimg.pl/246x138/" />
+          <div className={card.duration_min}>04:29</div>
+        </div>
+        <div className={card.content}>
+          <div className={card.title}>
+            <h3 className={card.headline}>
+              My Linux Ubuntu Setup for Software Development
+            </h3>
+            <div className={card.info}>ForrestKnight</div>
+            <div className={card.info}>45 тыс. просмотров</div>
+            <div className={card.info}> Неделю назад</div>
+          </div>
+          <div className={card.descr}>
+            A look into my Ubuntu software development environment VM I use at
+            work as a software engineer. The OS is Ubuntu Linux ...software
+            development environment VM I use at work as a software engineer.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoCardHor;
