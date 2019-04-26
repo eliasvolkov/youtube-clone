2401e7ed7b4a8195588876c10b0f315cfc9f3602
Ilia Volkov <ilya.j0b@yandex.ru>
2019-04-26 15:36:59 +0300

    create Videocard layout

---
+++ src/components/VideoCard/VideoCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import card from "./VideoCard.scss";
+
+const VideoCard = () => {
+  return (
+    <div className={card.container}>
+      <div className={card.card}>
+        <div className={card.img}>
+          <img src="https://fakeimg.pl/213x118/" />
+        </div>
+        <h3 className={card.headline}>
+          My Linux Ubuntu Setup for Software Development
+        </h3>
+        <div className={card.author}>ForrestKnight</div>
+        <div className={card.views}>просмотров 45 тыс.</div>
+        <div className={card.time}> Неделю назад</div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoCard;
