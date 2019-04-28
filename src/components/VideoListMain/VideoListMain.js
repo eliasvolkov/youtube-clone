9e5afd0e7b117e2fd5f289b45616415728384401
Ilia Volkov <ilya.j0b@yandex.ru>
2019-04-28 15:51:44 +0300

    add data into VideoListMain

--- src/components/VideoListMain/VideoListMain.js
+++ src/components/VideoListMain/VideoListMain.js
@@ -4,21 +4,28 @@ import VideoCard from "../VideoCard/VideoCard";
 
 export default class VideoListMain extends Component {
   render() {
-    return (
-      <div className={videolistm.container}>
-        <h4>Популярные</h4>
-        <div className={videolistm.row}>
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
+    if (this.props.videos === []) {
+      return null;
+    } else {
+      return (
+        <div className={videolistm.container}>
+          <h4>Популярные</h4>
+          <div className={videolistm.row}>
+            {this.props.videos.map(video => {
+              return (
+                <VideoCard
+                  key={video.id.videoId}
+                  img={video.snippet.thumbnails.medium.url}
+                  title={video.snippet.title}
+                  author={video.snippet.channelTitle}
+                  views={video.statistics.viewCount}
+                  click={() => this.props.click(video)}
+                />
+              );
+            })}
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
