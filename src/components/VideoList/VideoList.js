e6e8309afd9da44f09ebacf241a5f28c35cdf7c1
Ilia Volkov <ilya.j0b@yandex.ru>
2019-04-28 14:24:31 +0300

    add functions to VideoList for worcking with data

--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -1,21 +1,41 @@
-import React, { Component } from "react";
-import videolist from "./VideoList.scss";
+import React from "react";
 import VideoCardHor from "../VideoCardHor/VideoCardHor";
+import videolist from "./VideoList.scss";
 
-export default class VideoList extends Component {
-  render() {
+function VideoList({ videos, click }) {
+  if (videos === []) {
     return (
       <div className={videolist.container}>
-        <h4>Результат</h4>
+        <div className={videolist.row} />
+      </div>
+    );
+  } else {
+    return (
+      <div className={videolist.container}>
         <div className={videolist.row}>
-          <VideoCardHor />
-          <VideoCardHor />
-          <VideoCardHor />
-          <VideoCardHor />
-          <VideoCardHor />
-          <VideoCardHor />
+          {videos.map(video => {
+            let img;
+            if (video.snippet.thumbnails == undefined) {
+              img = "alt";
+              return img;
+            } else {
+              img = video.snippet.thumbnails.medium.url;
+            }
+            return (
+              <VideoCardHor
+                key={video.id.videoId}
+                img={img}
+                title={video.snippet.title}
+                author={video.snippet.channelTitle}
+                descr={video.snippet.description}
+                click={() => click(video)}
+              />
+            );
+          })}
         </div>
       </div>
     );
   }
 }
+
+export default VideoList;
