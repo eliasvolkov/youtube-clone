f41aaeb7760c28a3426a102df8a086e208cad43e
Ilia Volkov <ilya.j0b@yandex.ru>
2019-04-28 14:23:38 +0300

    add data into VideoDetail

--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -1,26 +1,28 @@
 import React from "react";
 import video from "./VideoDetail.scss";
 
-function VideoDetail() {
-  return (
-    <div className={video.container}>
-      <div className={video.video}>
-        <iframe
-          height="480"
-          src="https://www.youtube.com/embed/bhopW92Qxw8"
-          frameborder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        />
+function VideoDetail({ id, video }) {
+  if (id == "" || video == null) {
+    return null;
+  } else {
+    return (
+      <div className={video.container}>
+        <div className={video.video}>
+          <iframe
+            height="480"
+            src={`https://www.youtube.com/embed/${id}`}
+            frameborder="0"
+            allow="autoplay"
+            allowfullscreen="allowfullscreen"
+          />
+        </div>
+        <div className={video.text}>
+          <h4 className={video.headline}>{video.snippet.title}</h4>
+          <p className={video.info}>48 986 просмотров</p>
+        </div>
       </div>
-      <div className={video.text}>
-        <h4 className={video.headline}>
-          My Linux Ubuntu Setup for Software Development
-        </h4>
-        <p className={video.info}>48 986 просмотров</p>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default VideoDetail;
