b159272942ddf94b1781dc0504954bc4d7ae0120
Ilia Volkov <ilya.j0b@yandex.ru>
2019-04-28 15:52:17 +0300

    create api call functions

--- src/servises/youtube.js
+++ src/servises/youtube.js
@@ -1,12 +1,13 @@
 export default class YouTube {
   constructor() {
     this.api_key = "AIzaSyAqO8_RkQkvaQ_s3th0qlRYB-0oRmsSIAo";
+    this.api_key1 = "AIzaSyCoeI5EbIirbYMCf-xl6XoZp1sQ7I5GFJQ";
   }
 
   async findVideos(title) {
     const response = await fetch(
       `https://www.googleapis.com/youtube/v3/search?q=${title}&key=${
-        this.api_key
+        this.api_key1
       }&maxResults=35&part=snippet`
     );
     const videos = await response.json();
@@ -15,8 +16,8 @@ export default class YouTube {
 
   async popularVideos() {
     const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=35&regionCode=US&key=${
-        this.api_key
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&chart=mostPopular&maxResults=35&regionCode=US&key=${
+        this.api_key1
       }`
     );
     const videos = await response.json();
